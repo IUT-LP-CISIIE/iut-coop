3cce566cf92e0fe186d26a678e550a2e9226ceaf
SOPRESS <gilles@sopress.net>
2017-12-05 17:04:48 +0100

    coop etape1: login/logout

---
+++ src/Appli.vue
@@ -0,0 +1,31 @@
+<template>
+	<div class="coop_appli">
+		vous êtes connecté en tant que <b>{{ nom }}</b>
+		<button class="button is-text" @click="logOut()">Se déconnecter</button>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'appli',
+		data () {
+			return {
+				nom:''
+			}
+		},
+		created() {
+			var member = JSON.parse(localStorage.getItem('member'));
+			this.nom = member.fullname;
+
+		},
+		methods: {
+			logOut() {
+				this.$emit('logout');
+			}
+		}
+	}
+</script>
+
+<style>
+
+</style>
